f4ea294eede09ebd03bd322116db7a813872696a
Mayank <shrivastavamayank01922@gmail.com>
2026-05-26 12:08:06 +0530

    Get started with React

---
+++ 01_reactvite/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+     <h1>Hello Mayank</h1>
+    )
+  }
+  
+  export default Chai
---
+++ 01_reactvite/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import Chai from './Chai.jsx'
+// import App from './App.jsx'
+
+
+export function Arpp() {
+    return (
+        
+     <h1>Hello React!</h1>
+    )
+  }
+  
+
+  
+createRoot(document.getElementById('root')).render(
+  
+    <Arpp/>
+ 
+)
