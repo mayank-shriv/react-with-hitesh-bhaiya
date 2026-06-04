d8defd050a7023d86b80428c0c55c455c842adca
Mayank <shrivastavamayank01922@gmail.com>
2026-06-04 15:44:46 +0530

    Task done

--- bg-color-changer/src/App.jsx
+++ bg-color-changer/src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive")
 
   return (
-    <>
-      
-    </>
+    <div className="w-full h-screen duration-200" style={{ backgroundColor: color }}>
+
+      <div className="duration-200" bg>
+  
+        <button onClick={()=>{setColor('Black')}}>black</button>
+        </div>
+      <div className="duration-200" bg>
+  
+        <button onClick={()=>{setColor('yellow')}}>yellow</button>
+      </div>
+      <div className=" duration-200" bg>
+  
+        <button onClick={()=>{setColor('blue')}}>Blue</button>
+      </div>
+      <div className="w-full h-screen duration-200" bg>
+  
+        <button onClick={()=>{setColor('Pink')}}>pink</button>
+        </div>
+     
+    </div>
   )
 }
 
